b6f1c7bd7d30c1778d0f3e55ad54b89fe33fc312
syso-anjarul <anjarulh168@gmail.com>
2025-11-03 15:39:15 +0530

    master push

---
+++ tests/demo111.spec.js
@@ -0,0 +1,10 @@
+const test = require("node:test");
+
+test("Demo test 111", async (t) => {
+  await t.test("Subtest 1", async () => {
+    // Test logic for Subtest 1
+  });
+    await t.test("Subtest 2", async () => {
+    // Test logic for Subtest 2
+  });
+});
